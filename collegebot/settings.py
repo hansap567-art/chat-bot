17bd3152a99037112de564b6c2c6b459736c03f4
Hansa P <hansap567@gmail.com>
2026-07-25 11:05:33 +0530

    have updated the entire code for the smooth and proper working

--- collegebot/settings.py
+++ collegebot/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
